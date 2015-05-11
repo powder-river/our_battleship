b6e57ce6654a2f271327b55ac97893ccbb9835fd
Danai Adkisson <dadkisso@live.com>
2015-05-11 16:20:00 -0400

    uhhhh

--- battleship.rb
+++ battleship.rb
@@ -18,11 +18,12 @@ class Ship
 
   def covers? (column, row)
 
+
   end
 
-
-
-
-
-
 end
+
+#game_hash = { A: 1, B: 2, C: 3, D: 4, E: 5,
+#F: 6, G: 7, H: 8, J: 9,}
+
+#gameboard = ShipCoverage.new (game_hash)
