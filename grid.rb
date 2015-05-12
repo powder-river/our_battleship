bd971da24ff32d163250756dfcde13aba10ec3fb
Danai Adkisson <dadkisso@live.com>
2015-05-12 15:07:10 -0400

    created grid to  test #20

--- grid.rb
+++ grid.rb
@@ -14,19 +14,36 @@ class Grid
   def display
 
 puts %Q{    1   2   3   4   5   6   7   8   9   10
-  -----------------------------------------
-A |   |   |   |   |   |   |   |   |   |   |
-B |   |   |   |   |   |   |   |   |   |   |
-C |   |   |   |   |   |   |   |   |   |   |
-D |   |   |   |   |   |   |   |   |   |   |
-E |   |   |   |   |   |   |   |   |   |   |
-F |   |   |   |   |   |   |   |   |   |   |
-G |   |   |   |   |   |   |   |   |   |   |
-H |   |   |   |   |   |   |   |   |   |   |
-I |   |   |   |   |   |   |   |   |   |   |
-J |   |   |   |   |   |   |   |   |   |   |
-  -----------------------------------------
-}
+  -----------------------------------------}
+
+
+    10.times do |i|
+      game_hash = { A: 1, B: 2, C: 3, D: 4, E: 5,
+      F: 6, G: 7, H: 8, I: 9, J: 10}
+      output = ""
+      row = i + 1
+      10.times do |j|
+        column = j + 1
+        fill = []
+        @holes.each do |k|
+          if k == [column,row]
+            fill << true
+          else
+            fill << false
+          end
+
+
+        end
+        if fill.include?(true)
+          output << "| O "
+        else
+          output << "|   "
+        end
+      end
+      puts game_hash.key(row).to_s + " " + output  + "|"
+    end
+    puts "  -----------------------------------------"
+
 
 
 
